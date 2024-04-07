4e75b2a1de6f2c87484cb22acc1144b4a7aee7f7
noor <aniiahmed380@gmail.com>
2024-04-08 04:33:42 +0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+
+type ansType ={
+    UserGuess: number
+}
+
+ const randomNumber=Math.floor( Math.random() * 10);
+
+ const ans : ansType = await inquirer.prompt([
+    {
+        type:"number",
+        name:"UserGuess",
+        message:"write your guess b/w 1 to 10"
+    }
+ ]);
+// destructure
+ const {UserGuess} = ans;
+
+ if( UserGuess === randomNumber){
+    console.log( UserGuess , 'UserGuess' , randomNumber , 'Random Number')
+    console.log("you won!!!!");
+    
+ }else{
+    console.log("plzz try again later \n better luck next time");
+    
+ }
+
+ console.log(ans.UserGuess);
+ 
